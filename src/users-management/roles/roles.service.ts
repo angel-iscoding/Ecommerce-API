7476290dfbf3dd484668c036a6144395ab0de07d
angel-iscoding <angel.iscoding@gmail.com>
2025-10-19 16:04:24 -0500

    Added: Better practices and refactoring. New method to assign roles to users

--- src/users-management/roles/roles.service.ts
+++ src/users-management/roles/roles.service.ts
@@ -3,11 +3,16 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { RolesRepository } from './roles.repository';
 import { Role } from '@/database/entities/role.entity';
 import { RoleNames } from '@/config/role-names.enum';
+import { UsersService } from '@/users-management/users/users.service';
+import { UserBaseResponseDto } from '@/database/dto/response/user-base-response.dto';
 
 @Injectable()
 export class RolesService {
-  constructor(private RolesRepository: RolesRepository) {}
-
+  constructor(
+    private RolesRepository: RolesRepository,
+    private UsersService: UsersService,
+  ) {}
+  
   async findAll(): Promise<Role[]> {
     return await this.RolesRepository.findAll();
   }
@@ -19,25 +24,37 @@ export class RolesService {
   
     return role;
   }
-
+  
   async findByName(name: RoleNames): Promise<Role | null> {
     const role = await this.RolesRepository.findByName(name);
-
+    
     if (!role) throw new NotFoundException('Role not found');
-
+    
     return role;
   }
-
+  
+  async create(name: RoleNames): Promise<Role> {
+    return this.RolesRepository.create(name);
+  }
+  
   async thisRoleExist(name: RoleNames): Promise<boolean> {
     const role = await this.RolesRepository.findByName(name);
     return !!role;
   }
-
+  
+  async assignRole(userId: string, roleId: string): Promise<UserBaseResponseDto> {
+    const user = await this.UsersService.findById(userId);
+    const role = await this.findById(roleId);
+    
+    user.role = role;
+    const updatedUser = await this.UsersService.update(user.id, user);
+  
+    return updatedUser;
+  }
+  
   async getDefaultRole(): Promise<Role> {
     return this.RolesRepository.findByName(RoleNames.User);
   }
 
-  async create(name: RoleNames): Promise<Role> {
-    return this.RolesRepository.create(name);
-  }
+
 }
