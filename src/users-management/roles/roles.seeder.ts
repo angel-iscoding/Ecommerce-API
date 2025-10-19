5902850d9d2f4e91183a2ce2b99d7e0f458b9593
angel-iscoding <angel.iscoding@gmail.com>
2025-10-19 14:34:19 -0500

    The seeder checks the role instead of find it

--- src/users-management/roles/roles.seeder.ts
+++ src/users-management/roles/roles.seeder.ts
@@ -2,7 +2,6 @@
 import { Injectable } from '@nestjs/common';
 import { RoleNames } from '@/config/role-names.enum';
 import { RolesService } from './roles.service';
-import { Role } from '@/database/entities/role.entity';
 
 @Injectable()
 export class RolesSeeder {
@@ -12,8 +11,8 @@ export class RolesSeeder {
     const rolesToSeed = Object.values(RoleNames);
     
     for (const roleName of rolesToSeed) {
-      const existingRole: Role | null = await this.RolesService.findByName(roleName)
-      
+      const existingRole = await this.RolesService.thisRoleExist(roleName)
+
       if (!existingRole) {
         await this.RolesService.create(roleName);
       }
