64f8e2e7f46e5b5669ba4f565ddeb566f1711b96
angel-iscoding <angel.iscoding@gmail.com>
2025-10-18 19:12:35 -0500

    Removing old files of DTO and Entities

--- src/database/cart/migrateCartDto.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class MigrateCartDto {
-  @ApiProperty({
-    description: 'ID temporal del usuario no autenticado',
-    example: 'temp123',
-  })
-  temporaryUserId: string;
-}
