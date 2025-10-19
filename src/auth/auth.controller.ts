a00f3284073b5ce1704dc7d41a42b8a6bdff8fc4
angel-iscoding <angel.iscoding@gmail.com>
2025-10-19 14:17:25 -0500

    Issue fixed: The controller was handling business logic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,4 @@
-import { LoginDto } from '@/database/dto/login.dto';
-import { RegisterDto } from '@/database/dto/register.dto';
-import { User } from '@/database/entities/user.entity';
+import { CreateUserRequestDto } from '@/database/dto/request/create-user-request.dto';
 import {
   BadRequestException,
   Body,
@@ -10,50 +8,35 @@ import {
   Post,
   Request,
 } from '@nestjs/common';
-import { CustomLogger } from '@/utils/logger/custom-logger.module';
 import { AuthService } from './auth.service';
-import { RoleService } from '@/user-management/roles/role.service';
-import { ContectDto } from '@/database/dto/content.dto';
+import { ApiResponseDto } from '@/database/dto/response/api-base-response.dto';
+import { LoginUserRequestDto } from '@/database/dto/request/login-user-request.dto';
 
 @Controller('auth')
 export class AuthController {
-  private readonly logger = new CustomLogger();
-
   constructor(
     private readonly authService: AuthService,
-    private readonly rolesService: RoleService,
   ) {}
 
   @Post('register')
   async register(
-    @Request() req,
-    @Body() user: RegisterDto,
-  ): Promise<ContectDto> {
-    this.logger.logRequest(req);
+    @Body() user: CreateUserRequestDto,
+  ): Promise<ApiResponseDto> {
     try {
-      if (user.password !== user.confirmPassword) {
-        throw new BadRequestException('Las contraseñas no coinciden');
-      }
-
-      const role = await this.rolesService.getRoleById(1);
-
-      if (role === null) {
-        throw new BadRequestException("Role does't exist");
-      }
-
-      const userCreated: User = await this.authService.register(user, role);
+      const userCreated = await this.authService.register(user);
       
       return {
         status: 'success',
         message: 'User created successfully',
         data: {
           user: userCreated,
+          //Agregar token de verificacion
         },
       };
     
     } catch (error) {
       throw new BadRequestException(
-        'No se pudo crear el usuario. Error: ' + error.message,
+        'Warning: ' + error.message,
       );
     }
   }
@@ -61,21 +44,23 @@ export class AuthController {
   @Post('login')
   @HttpCode(HttpStatus.OK)
   async login(
-    @Request() req,
-    @Body() loginUserDto: LoginDto,
-  ): Promise<ContectDto> {
-    this.logger.logRequest(req);
-    const { email, password } = loginUserDto;
-    const token = await this.authService.login({email, password});
-
-    if (!token) {
-      throw new BadRequestException('Email o contraseña incorrectos');
+    @Body() loginUserDto: LoginUserRequestDto,
+  ): Promise<ApiResponseDto> {
+    try {
+      const token = await this.authService.login(loginUserDto);
+    
+      if (!token) {
+      throw new BadRequestException('Email or password incorrect');
     }
-
     return {
       status: 'success',
       message: 'User logged in successfully',
       data: { access_token: token },
     };
+    } catch (error) {
+      throw new BadRequestException(
+        'Warning: ' + error.message,
+      );
+    }
   }
 }
