6cd1052ed9040cdb8d3af3b47abe5dc0732e8dd2
angel-iscoding <angel.iscoding@gmail.com>
2025-10-18 19:23:33 -0500

    Changed type to the new Product type

--- src/cloud/cloud.controller.ts
+++ src/cloud/cloud.controller.ts
@@ -1,7 +1,15 @@
-import { Controller, Param, Post, UploadedFile } from '@nestjs/common';
+import {
+  Controller,
+  Param,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { CloudService } from './cloud.service';
 import { ProductsService } from '../store-management/products/product.service';
 import { FileValidationPipe } from '../utils/pipes/file-validation.pipe';
+import { FileInterceptor } from '@nestjs/platform-express';
 import {
   ApiTags,
   ApiOperation,
@@ -24,9 +32,10 @@ export class CloudController {
   @ApiResponse({ status: 400, description: 'Bad request' })
   @ApiConsumes('multipart/form-data')
   @ApiParam({ name: 'id', description: 'Product ID' })
+  @UseInterceptors(FileInterceptor('file'))
   async uploadImage(
-    @UploadedFile(FileValidationPipe) file: Express.Multer.File,
-    @Param('id') productId: string,
+    @UploadedFile(new FileValidationPipe()) file: Express.Multer.File,
+    @Param('id', ParseIntPipe) productId: number,
   ) {
     const imageUrl = await this.cloudService.uploadImage(file);
     await this.productsService.updateProductImage(productId, imageUrl);
