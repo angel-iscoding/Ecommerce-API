2a3a9e380469765c2d3a3858bde6c9dd7d0c1c37
angel-iscoding <angel.iscoding@gmail.com>
2025-10-24 07:41:33 -0500

    Removed: Assing Role methods and moved to Users Module

--- src/users-management/roles/roles.service.ts
+++ src/users-management/roles/roles.service.ts
@@ -3,14 +3,11 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { RolesRepository } from './roles.repository';
 import { Role } from '@/database/entities/role.entity';
 import { RoleNames } from '@/config/role-names.enum';
-import { UsersService } from '@/users-management/users/users.service';
-import { UserBaseResponseDto } from '@/database/dto/response/user-base-response.dto';
 
 @Injectable()
 export class RolesService {
   constructor(
-    private RolesRepository: RolesRepository,
-    private UsersService: UsersService,
+    private RolesRepository: RolesRepository
   ) {}
   
   async findAll(): Promise<Role[]> {
@@ -42,19 +39,7 @@ export class RolesService {
     return !!role;
   }
   
-  async assignRole(userId: string, roleId: string): Promise<UserBaseResponseDto> {
-    const user = await this.UsersService.findById(userId);
-    const role = await this.findById(roleId);
-    
-    user.role = role;
-    const updatedUser = await this.UsersService.update(user.id, user);
-  
-    return updatedUser;
-  }
-  
   async getDefaultRole(): Promise<Role> {
     return this.RolesRepository.findByName(RoleNames.User);
   }
-
-
 }
